d3912685654764f75b0738b3de1bab356861b80f
Miguel Aréjula <arejula10@gmail.com>
2024-02-21 13:10:41 +0000

    try change volume logo

--- playbeat/src/components/Player.jsx
+++ playbeat/src/components/Player.jsx
@@ -4,31 +4,31 @@ import {Slider} from "./Slider"
 
 
 export const Play = ({classname}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="38" height="38" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="white" strokeLinecap="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" strokeWidth="0" fill="white" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="white" strokeLinecap="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" strokeWidth="0" fill="white" /></svg>
 )
 
 
 export const Pause = ({classname}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="38" height="38" viewBox="0 0 24 24" strokeWidth="1.5" stroke="none" fill="white" strokeLinecap="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 5h4v14h-4z" /><path d="M14 5h4v14h-4z" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="none" fill="white" strokeLinecap="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 5h4v14h-4z" /><path d="M14 5h4v14h-4z" /></svg>
 )
 
 export const NextSong = ({classname}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" strokeWidth="0" fill="white" /><path d="M13 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" strokeWidth="0" fill="white" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" strokeWidth="0" fill="white" /><path d="M13 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z" strokeWidth="0" fill="white" /></svg>
 )
 
 export const PreviousSong = ({classname}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20.341 4.247l-8 7a1 1 0 0 0 0 1.506l8 7c.647 .565 1.659 .106 1.659 -.753v-14c0 -.86 -1.012 -1.318 -1.659 -.753z" strokeWidth="0" fill="white" /><path d="M9.341 4.247l-8 7a1 1 0 0 0 0 1.506l8 7c.647 .565 1.659 .106 1.659 -.753v-14c0 -.86 -1.012 -1.318 -1.659 -.753z" strokeWidth="0" fill="white" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={classname} width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20.341 4.247l-8 7a1 1 0 0 0 0 1.506l8 7c.647 .565 1.659 .106 1.659 -.753v-14c0 -.86 -1.012 -1.318 -1.659 -.753z" strokeWidth="0" fill="white" /><path d="M9.341 4.247l-8 7a1 1 0 0 0 0 1.506l8 7c.647 .565 1.659 .106 1.659 -.753v-14c0 -.86 -1.012 -1.318 -1.659 -.753z" strokeWidth="0" fill="white" /></svg>
 )
 
 export const VolumeMute = ({classname}) => (
-<svg xmlns="http://www.w3.org/2000/svg" className={classname} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /><path d="M16 10l4 4m0 -4l-4 4" /></svg>
+<svg xmlns="http://www.w3.org/2000/svg" className={classname} width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /><path d="M16 10l4 4m0 -4l-4 4" /></svg>
 )
 
 export const VolumeMid = ({classname}) => (
-  <svg xmlns="http://www.w3.org/2000/svg" class={classname} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" class={classname} width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg>
 )
 export const VolumeHigh = ({classname}) => (
-  <svg xmlns="http://www.w3.org/2000/svg" class={classname} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" class={classname} width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 0 8" /><path d="M17.7 5a9 9 0 0 1 0 14" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /></svg>
   )
 
 export const CancionActual = ({image, title,artists}) => {
@@ -56,11 +56,47 @@ export const CancionActual = ({image, title,artists}) => {
 
 
 export const SongBar = () => {
- 
-
+  return (
+    <div className="flex flex-row items-center gap-2 mb-3">
+      
+      <Slider defaultValue={[80]}
+      max={100}
+      min={0}
+      className="w-[440px]"
+      onValueChange={(value) => {console.log(value)}}
+      />
+    </div>
+  )
 }
 
-export const VolumeControl = () => {
+export const VolumeControl = ({audio}) => {
+  return (
+    <div className="text-white flex flex-row items-center w-[200px]">
+             {/* Add the volume control here */}
+            <div className="flex flex-row items-center gap-2">
+              <button>
+              {/* {audio.current.volume > 0.5 ? (
+                  <VolumeHigh className={"hover:opacity-100 opacity-70 transition"} />
+                ) : audio.current.volume > 0.2 ? (
+                  <VolumeMid className={"hover:opacity-100 opacity-70 transition"} />
+                ) : (
+                  <VolumeMute className={"hover:opacity-100 opacity-70 transition"} />
+                )} */}
+              </button>
+              <Slider 
+              defaultValue={[100]}
+              max={100}
+              min={0}
+              className="w-20"
+              onValueChange={(value) => {
+                const [nuevoVol] = value
+                 audio.current.volume = nuevoVol / 100
+              }}
+
+              />
+            </div>
+        </div>
+  )
   
 }
 
@@ -95,7 +131,7 @@ export function Player () {
 
         <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center flex-col items-center">
-            <div className="flex flex-row">
+            <div className="flex flex-row ">
 
               <button>
                   <PreviousSong classname={"hover:opacity-100 opacity-70 transition"} />
@@ -106,33 +142,15 @@ export function Player () {
               <button>
                 <NextSong classname={"hover:opacity-100 opacity-70 transition"}/>
               </button>
-              <SongBar />
-              <audio ref={audio} />
+              
             </div>
-          
+            <SongBar />
+            <audio ref={audio} />
         </div>
 
       </div>
-       
-        <div className="text-white flex flex-row items-center w-[200px]">
-             {/* Add the volume control here */}
-            <div className="flex flex-row items-center gap-2">
-              <button>
-                <VolumeMute classname={"hover:opacity-100 opacity-70 transition"}/>
-              </button>
-              <Slider 
-              defaultValue={[100]}
-              max={100}
-              min={0}
-              className="w-20"
-              onValueChange={(value) => {
-                const [nuevoVol] = value
-                 audio.current.volume = nuevoVol / 100
-              }}
-
-              />
-            </div>
-        </div>
+       <VolumeControl audio={audio}/>
+        
     </div>
   )
 }
