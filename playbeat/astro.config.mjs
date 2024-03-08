656debf05d491fd38fc7aa098c04664869ab5b50
pabloangusto <pabloangusto@gmail.com>
2024-03-08 13:19:27 +0100

    Add Queue component and update Tabs component

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [tailwind(), react(), svelte(), vue()]
 });
---
+++ playbeat/src/components/Queue.vue
@@ -0,0 +1,31 @@
+
+
+<script setup>
+import {ref} from 'vue'
+
+const canciones = ref([{nombre: 'Cancion 1', duracion: '3:20'}, {nombre: 'Cancion 2', duracion: '3:20'}, {nombre: 'Cancion 3', duracion: '3:20'}, {nombre: 'Cancion 4', duracion: '3:20'}, {nombre: 'Cancion 5', duracion: '3:20'}, {nombre: 'Cancion 6', duracion: '3:20'}, {nombre: 'Cancion 7', duracion: '3:20'}, {nombre: 'Cancion 8', duracion: '3:20'}, {nombre: 'Cancion 9', duracion: '3:20'}, {nombre: 'Cancion 10', duracion: '3:20'}, {nombre: 'Cancion 11', duracion: '3:20'}, {nombre: 'Cancion 12', duracion: '3:20'}, {nombre: 'Cancion 13', duracion: '3:20'}, {nombre: 'Cancion 14', duracion: '3:20'}, {nombre: 'Cancion 15', duracion: '3:20'}, {nombre: 'Cancion 16', duracion: '3:20'}, {nombre: 'Cancion 17', duracion: '3:20'}, {nombre: 'Cancion 18', duracion: '3:20'}, {nombre: 'Cancion 19', duracion: '3:20'}, {nombre: 'Cancion 20', duracion: '3:20'}, {nombre: 'Cancion 21', duracion: '3:20'}, {nombre: 'Cancion 22', duracion: '3:20'}, {nombre: 'Cancion 23', duracion: '3:20'}, {nombre: 'Cancion 24', duracion: '3:20'}, {nombre: 'Cancion 25', duracion: '3:20'}, {nombre: 'Cancion 26', duracion: '3:20'}, {nombre: 'Cancion 27', duracion: '3:20'}, {nombre: 'Cancion 28', duracion: '3:20'}, {nombre: 'Cancion 29', duracion: '3:20'}, {nombre: 'Cancion 30', duracion: '3:20'}, {nombre: 'Cancion 31', duracion: '2.10'}])
+
+ 
+</script>
+<template >
+    <section class="flex w-full h-full flex-col justify-between">
+        <ul class="flex flex-col px-3 mt-6 h-[90%] overflow-y-scroll">
+            <li v-for="item of canciones" class="flex flex-row gap-4">
+                <div class=" text-white  font-medium flex flex-row items-center justify-center w-full h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-playlist" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M14 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                        <path d="M17 17v-13h4" />
+                        <path d="M13 5h-10" />
+                        <path d="M3 9l10 0" />
+                        <path d="M9 13h-6" />
+                    </svg>
+                    <span class="ml-4">{{ item.nombre }}</span>
+                    <span class="ml-10">{{item.duracion}}</span>
+                </div>
+            </li>
+        </ul>
+        <button @click="canciones.pop()" class=" self-center w-max py-1 px-5 border-2 rounded-md hover:border-[#fc0307] transition-colors">Eliminar</button>
+    </section>
+    
+</template>
