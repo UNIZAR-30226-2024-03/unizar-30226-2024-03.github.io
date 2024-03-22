eebca9759a12b90b0a2a300bd1493fba53b40bb6
Miguel Aréjula <arejula10@gmail.com>
2024-03-22 17:11:25 +0000

    Add new files and make code improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://UNIZAR-30226-2024-03.github.io',
-  base: '/web/playbeat',
+  base: '/web',
   integrations: [tailwind(), react(), svelte(), vue()]
 });
