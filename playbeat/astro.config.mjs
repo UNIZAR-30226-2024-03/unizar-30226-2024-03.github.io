539b405f255d8562d10ef0afb9346608bcf8de81
Miguel Aréjula <arejula10@gmail.com>
2024-03-23 11:29:04 +0000

    Update astro.config.mjs file

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -3,14 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
+
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unizar-30226-2024-03.github.io',
-  base: '/Web',
+  base: '/unizar-30226-2024-03.github.io/',
   integrations: [tailwind(), react(), svelte(), vue()],
-  output: "server",
-  adapter: vercel()
+
   
 });
