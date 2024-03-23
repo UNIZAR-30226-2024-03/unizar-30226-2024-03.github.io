7c1e5c8b6c1c02468ba49c9f6ae23907a120f35b
Miguel Aréjula Aisa <92888725+Arejula11@users.noreply.github.com>
2024-03-23 12:06:31 +0100

    base url

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -8,7 +8,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unizar-30226-2024-03.github.io',
-  base: '/',
+  base: '/Web',
   integrations: [tailwind(), react(), svelte(), vue()],
   
 });
