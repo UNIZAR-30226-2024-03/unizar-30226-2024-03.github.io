3dd4ec0cec5578b71d673b691fea2aa9226e1d77
pabloangusto <pabloangusto@gmail.com>
2024-03-23 13:23:50 +0100

    Update astro.config.mjs with site and base URLs

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -8,9 +8,9 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://unizar-30226-2024-03.github.io',
-  // base: '/unizar-30226-2024-03.github.io/',
+   site: 'https://unizar-30226-2024-03.github.io',
+   base: '/unizar-30226-2024-03.github.io/',
   integrations: [tailwind(), react(), svelte(), vue()],
-  output: "server",
-  adapter: vercel()
+  //output: "server",
+  //adapter: vercel()
 });
