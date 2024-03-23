64e5c0435b8657ff01ffe1fb84fd48bc34e3d2dd
Miguel Aréjula <arejula10@gmail.com>
2024-03-23 11:31:21 +0000

    Add @astrojs/vercel package and configure Astro to use Vercel adapter

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unizar-30226-2024-03.github.io',
   base: '/unizar-30226-2024-03.github.io/',
   integrations: [tailwind(), react(), svelte(), vue()],
-
-  
+  output: "server",
+  adapter: vercel()
 });
