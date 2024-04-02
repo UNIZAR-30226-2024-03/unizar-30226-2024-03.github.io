7f39cdef1d36630ec748f3274e9e094559ce967b
Miguel Aréjula <arejula10@gmail.com>
2024-04-02 15:12:02 +0000

    Update GitHub Pages deployment workflow and Astro configuration

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -4,12 +4,12 @@ import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
-//import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 
 export default defineConfig({
-  site: 'https://unizar-30226-2024-03.github.io',
+  // site: 'https://unizar-30226-2024-03.github.io',
   integrations: [tailwind(), react(), svelte(), vue()],
   output: "server",
-  // adapter: vercel()
+  adapter: vercel()
 });
