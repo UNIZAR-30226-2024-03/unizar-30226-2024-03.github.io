5f8c55fa9bfaa719817be0ff351dccfea3a36458
Miguel Aréjula <arejula10@gmail.com>
2024-04-04 08:01:03 +0000

    registro

--- playbeat/src/utils/register.ts
+++ playbeat/src/utils/register.ts
@@ -1,14 +1,12 @@
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
-async function registerUser(data: { name: any; email: any; password: any; }) {
+async function registerUser(data: { nombreUsuario: any; email: any; contrasegna: any; }) {
   try {
-    console.log("Sending request with data:", data);
+
     const response = await axios.post(Global.url + "auth/signup", data);
 
-    console.log("Response:", response.data);
-
-    return response.data;
+    return response;
   } catch (error) {
     console.error("Error registering user:", error);
     throw error;
