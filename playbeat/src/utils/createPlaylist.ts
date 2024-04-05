37869d3f168c0c4892a9ae59874b7efe1fbc7579
pabloangusto <pabloangusto@gmail.com>
2024-04-05 10:36:44 +0200

    Add playlist creation functionality

---
+++ playbeat/src/utils/createPlaylist.ts
@@ -0,0 +1,16 @@
+import { Global } from "@/globalState/globalUrl.js";
+import axios from 'axios';
+
+async function createPlaylist(data: { nombre: string; descripcion: string; esPrivada: boolean; imgLista: string; esAlbum: boolean; tipoLista: any; idUsuario: number; audios: number[];}, token: any) {
+    try {
+        axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+        const response = await axios.post(Global.url + "lista", data);
+
+        return response;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
+}
+
+export { createPlaylist };
