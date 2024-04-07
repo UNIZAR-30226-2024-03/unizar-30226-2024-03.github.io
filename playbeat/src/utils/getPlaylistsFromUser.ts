41968197e8578a5535479f5807442a200d65bf91
pabloangusto <pabloangusto@gmail.com>
2024-04-07 19:08:37 +0200

    Refactor LeftBar component and update getPlaylistsFromUser function

--- playbeat/src/utils/getPlaylistsFromUser.ts
+++ playbeat/src/utils/getPlaylistsFromUser.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 async function getPlaylistsFromUser( token: any) {
     try {
         axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
-        const response = await axios.post(Global.url + "lista/owned/308");
+        const response = await axios.get(Global.url + "lista/owned/308");
 
         return response;
     } catch (error) {
