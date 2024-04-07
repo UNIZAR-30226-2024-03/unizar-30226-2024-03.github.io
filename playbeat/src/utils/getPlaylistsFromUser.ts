b13602855ae4eae0c41020b1eab75e26347003ee
pabloangusto <pabloangusto@gmail.com>
2024-04-07 18:25:40 +0200

    Add token authentication and get playlists from user

---
+++ playbeat/src/utils/getPlaylistsFromUser.ts
@@ -0,0 +1,16 @@
+import { Global } from "@/globalState/globalUrl.js";
+import axios from 'axios';
+
+async function getPlaylistsFromUser( token: any) {
+    try {
+        axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+        const response = await axios.post(Global.url + "lista/owned/308");
+
+        return response;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
+}
+
+export { getPlaylistsFromUser };
