0b43f2ce4727f9e1016dc672da6cc3eac5936f62
Miguel Aréjula <arejula10@gmail.com>
2024-04-08 12:04:31 +0000

    Update user data retrieval and display in editProfile.astro component

---
+++ playbeat/src/utils/editUser.ts
@@ -0,0 +1,25 @@
+
+import { Global } from "@/globalState/globalUrl.js";
+
+import axios from 'axios';
+
+async function editUser(jws: string) {
+    try {
+
+        const response = await axios.put(Global.url + "usuario",{
+            headers: {
+                Authorization: `Bearer ${jws}`,
+            },
+            data: {
+                
+            }
+        });
+
+        return response;
+    } catch (error) {
+        console.error("Error geting user data:", error);
+        throw error;
+    }
+}
+
+export { editUser };
