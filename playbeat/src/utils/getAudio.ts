71d60adc45464420310cda98eec9d6b05c3530fd
Miguel Aréjula <arejula10@gmail.com>
2024-04-09 14:36:55 +0000

    Update Player component to fetch audio using jws token

---
+++ playbeat/src/utils/getAudio.ts
@@ -0,0 +1,20 @@
+import { Global } from "@/globalState/globalUrl.js";
+
+import axios from 'axios';
+
+async function getAudio(jws: string, id:string) {
+    try {;
+        const response = await axios.get(Global.url + "audio/"+id,{
+            headers: {
+                Authorization: `Bearer ${jws}`,
+            },
+        });
+
+        return response;
+    } catch (error) {
+        console.error("Error geting user data:", error);
+        throw error;
+    }
+}
+
+export { getAudio };
