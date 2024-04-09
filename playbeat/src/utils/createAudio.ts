901306bdff378f18676ad5c890d14a9ec822fee2
pabloangusto <pabloangusto@gmail.com>
2024-04-09 16:47:10 +0200

    Update audio upload functionality and fix bug in duration calculation

--- playbeat/src/utils/createAudio.ts
+++ playbeat/src/utils/createAudio.ts
@@ -1,8 +1,9 @@
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
-async function createAudio(data: { titulo: string; descripcion: string; esPrivada: boolean; imgLista: string; esAlbum: boolean; tipoLista: any; idUsuario: number; audios: number[];}, token: any) {
+async function createAudio(data: {titulo: string;esPrivada: boolean;img: string;esAlbum: boolean;fechaLanz: number;duracionSeg: any}, token: any) {
     try {
+        console.log(data)
         axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
         const response = await axios.post(Global.url + "audio/upload", data);
 
