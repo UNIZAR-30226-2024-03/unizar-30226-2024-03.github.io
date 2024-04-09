9acb9cc09c781c2dda875be3d91b056dd042d735
pabloangusto <pabloangusto@gmail.com>
2024-04-09 13:59:11 +0200

    Refactor getInfoPlaylist function to accept any type for the id parameter

---
+++ playbeat/src/utils/editPlaylist.ts
@@ -0,0 +1,17 @@
+import { Global } from "@/globalState/globalUrl.js";
+import axios from 'axios';
+
+async function editPlaylist(data: { nombre: string; descripcion: string; esPrivada: boolean; imgLista: string; esAlbum: boolean; tipoLista: any; idUsuario: number; audios: number[]; Propietarios: number[]}, token: any, id: any) {
+    try {
+
+        axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+        const response = await axios.put(Global.url + "lista/" + id, {updateBody: data, id:id});
+
+        return response;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
+}
+
+export { editPlaylist };
--- playbeat/src/utils/getInfoPlaylist.ts
+++ playbeat/src/utils/getInfoPlaylist.ts
@@ -1,7 +1,7 @@
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
-async function getInfoPlaylist( id:number, token: any) {
+async function getInfoPlaylist( id:any, token: any) {
     try {
         axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
         const response = await axios.get(Global.url + "lista/extra/"+ id);
