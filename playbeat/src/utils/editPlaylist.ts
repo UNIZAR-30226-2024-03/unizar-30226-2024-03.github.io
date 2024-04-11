fadc23b43fb541e4347fc1f2fa8adfb0ed922ae8
pabloangusto <pabloangusto@gmail.com>
2024-04-11 19:08:56 +0200

    Refactor editPlaylist function to remove unused idUsuario parameter

--- playbeat/src/utils/editPlaylist.ts
+++ playbeat/src/utils/editPlaylist.ts
@@ -1,11 +1,11 @@
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
-async function editPlaylist(data: { nombre: string; descripcion: string; esPrivada: boolean; imgLista: string; esAlbum: boolean; tipoLista: any; idUsuario: number; audios: number[]; Propietarios: number[]}, token: any, id: any) {
+async function editPlaylist(data: { nombre: string; descripcion: string; esPrivada: boolean; imgLista: string; esAlbum: boolean; tipoLista: any;  audios: number[];}, token: any, id: any) {
     try {
 
         axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
-        const response = await axios.put(Global.url + "lista/" + id, {updateBody: data});
+        const response = await axios.put(Global.url + "lista/" + id,  data);
 
         return response;
     } catch (error) {
