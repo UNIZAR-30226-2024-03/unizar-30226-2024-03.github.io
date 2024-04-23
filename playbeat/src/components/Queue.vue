20e19a702be93f7527ebb05fa8c0e74875a6683d
pabloangusto <pabloangusto@gmail.com>
2024-04-23 10:17:24 +0000

    Refactor playlist loop logic and add songs to queue in Queue.vue and Layout.astro

--- playbeat/src/components/Queue.vue
+++ playbeat/src/components/Queue.vue
@@ -23,6 +23,7 @@ onMounted(() => {
     window.addEventListener("nextSong",(event) => {
         if (canciones.value.length > 0) {
             const primerElemento = canciones.value.shift();
+            console.log("nssss")
             window.localStorage.setItem('songsQueue', JSON.stringify(canciones.value));
             localStorage.setItem("cancion", primerElemento.id);
             return primerElemento;
@@ -34,9 +35,11 @@ onMounted(() => {
             const pl = JSON.parse(window.localStorage.getItem('playlist'));
             playlist.value = pl
         }
+        console.log("achis" + playlist)
         const primerElemento = playlist.value.shift();
         window.localStorage.setItem('playlistQueue', JSON.stringify(playlist.value));
         localStorage.setItem("cancion", primerElemento.id);
+        console.log("achis" + playlist)
 
     })
     
