74188a1718b136d28ee249cf8f1ddb951aab25c6
Miguel Aréjula <arejula10@gmail.com>
2024-04-23 13:30:59 +0000

    carrousesles

--- playbeat/src/interfaces.ts
+++ playbeat/src/interfaces.ts
@@ -2,3 +2,19 @@ export interface Etiqueta {
     idEtiqueta: number;
     nombre: string;
 }
+
+
+export interface Audio {
+    idAudio: number;
+    titulo: string;
+    path: string;
+    duracionSeg: number;
+    fechaLanz: string;
+    esAlbum: boolean;
+    imgAudio: string;
+    esPrivado: boolean;
+    esPodcast: boolean;
+    Artistas?: [];
+    etiquetas?: Etiqueta[]; 
+    vecesEscuchada: number; 
+}
