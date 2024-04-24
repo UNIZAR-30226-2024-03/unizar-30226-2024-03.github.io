a5279fb5eabe1939a48460bb81cf8f4f13b4e3a4
Garicore01 <848905@unizar.es>
2024-04-24 12:54:07 +0200

    Create the documentation for Tags

--- playbeat/src/utils/getEtiquetasCancion.ts
+++ playbeat/src/utils/getEtiquetasCancion.ts
@@ -1,3 +1,10 @@
+/**
+ * Obtiene las etiquetas disponibles para las canciones.
+ * 
+ * @param {any} token - Token de autenticación del usuario.
+ * @returns {Promise<object>} - Una promesa que se resuelve con el objeto de respuesta de la solicitud.
+ * @throws {Error} - Si la solicitud de inicio de sesión no se puede completar correctamente.
+ */
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
