33bda83c98c62d641a5f840be5ecfafb20e1fec1
pabloangusto <pabloangusto@gmail.com>
2024-04-27 12:22:15 +0000

    Refactor getStatsAudio function and update Stats3 component in Astro files

---
+++ playbeat/src/utils/getStatsAudio.ts
@@ -0,0 +1,16 @@
+import { Global } from "@/globalState/globalUrl.js";
+import axios from 'axios';
+
+async function getStatsAudio( token: any, id:number, data: { date: any}) {
+    try {
+        axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+        const response = await axios.post(Global.url + "audio/stats/" + id, data);
+
+        return response;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
+}
+
+export { getStatsAudio };
