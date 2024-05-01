602cd84c59b5ba5da04f4902379d1387cc289af2
Garicore01 <848905@unizar.es>
2024-05-01 20:26:42 +0200

    Set Google Auth

--- playbeat/src/utils/googleAuth.ts
+++ playbeat/src/utils/googleAuth.ts
@@ -1,5 +1,5 @@
 /**
- * Inicia sesión de un usuario enviando una solicitud POST en el servidor.
+ * Inicia sesión de un usuario mediante Google, enviando una solicitud POST en el servidor.
  * 
  * @param {object} response - La respuesta de Google Sign-In, incluye nombre, email e imagen del usuario.
  * @returns {Promise<object>} - Una promesa que se resuelve con el objeto de respuesta de la solicitud.
@@ -10,9 +10,10 @@ import axios from 'axios';
 
 async function sendInfo(response: any) {
     try {
-        const apiResponse = await axios.post(Global.url + "auth/login", response);
-        console.log('Response:', apiResponse);
-        return apiResponse;
+        //console.log('Response:', response);
+        const apiResponse = await axios.post(Global.url + "auth/google", response);
+        //console.log(apiResponse);
+        return apiResponse; // Devolver el JWT que devuelve el servidor.
     } catch (error) {
         console.error("Error logging in user:", error);
         throw error;
