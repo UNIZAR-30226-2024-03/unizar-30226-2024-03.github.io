cf0671fbad1ba51d70820039ab5d2c955f8f0c46
pabloangusto <pabloangusto@gmail.com>
2024-05-01 10:50:49 +0000

    Fix image upload issue in uploadImg.ts

--- playbeat/src/utils/uploadImg.ts
+++ playbeat/src/utils/uploadImg.ts
@@ -3,15 +3,17 @@ import axios from 'axios';
 
 async function uploadImg( image:any , token?:any) {
     try {
+        let headers: { [key: string]: string } = {
+            "Content-Type": "multipart/form-data"
+        };
+
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
+        }
         //console.log(image)
         let formData = new FormData();
         formData.append("image", image);
-        const response = await axios.post(Global.url + "foto/", formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",    
-                'Authorization': `Bearer ${token}`  
-            }
-            });
+        const response = await axios.post(Global.url + "foto/", formData, {headers});
         return response.data;
     } catch (error) {
         console.error("Error registering user:", error);
