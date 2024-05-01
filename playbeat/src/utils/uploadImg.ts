d8dfb00fa778bafed4f38186664e654de7f90b9d
pabloangusto <pabloangusto@gmail.com>
2024-05-01 10:35:15 +0000

    Fix image upload issue in ModAlbum.astro and uploadImg.ts

--- playbeat/src/utils/uploadImg.ts
+++ playbeat/src/utils/uploadImg.ts
@@ -1,14 +1,15 @@
 import { Global } from "@/globalState/globalUrl.js";
 import axios from 'axios';
 
-async function uploadImg(image:any) {
+async function uploadImg( image:any , token?:any) {
     try {
         //console.log(image)
         let formData = new FormData();
         formData.append("image", image);
         const response = await axios.post(Global.url + "foto/", formData, {
             headers: {
-                "Content-Type": "multipart/form-data",            
+                "Content-Type": "multipart/form-data",    
+                'Authorization': `Bearer ${token}`  
             }
             });
         return response.data;
