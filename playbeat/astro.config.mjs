9ea26c57cca88a9175379e1d4e5a3e24c85f2a65
pabloangusto <pabloangusto@gmail.com>
2024-05-02 14:31:31 +0000

    Add prefetch configuration to astro.config.mjs

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -11,5 +11,8 @@ export default defineConfig({
   // site: 'https://unizar-30226-2024-03.github.io',
   integrations: [tailwind(), react(), svelte(), vue()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  prefetch:{
+    prefetchAll: false
+  }
 });
