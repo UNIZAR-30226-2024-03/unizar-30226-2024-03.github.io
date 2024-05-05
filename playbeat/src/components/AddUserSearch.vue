06087d978c658239d31f218871891101141f341a
Miguel Aréjula <arejula10@gmail.com>
2024-05-05 11:45:43 +0000

    style

--- playbeat/src/components/AddUserSearch.vue
+++ playbeat/src/components/AddUserSearch.vue
@@ -59,7 +59,7 @@ const removeArtist = (id) => {
         <p v-if="error" class=" bg-[#ff0202] bg-opacity-60 rounded-md border-1 border-[#ff0202]">{{  error}}</p>
         <!-- Displaying list of artists -->
         <div v-for="artist in artistList" :key="artist.id" class="text-white bg-bluePB rounded-lg p-2 g-4 flex flex-row">
-            <span class="font-bold">{{ artist.name }}</span>
+            <span >{{ artist.name }}</span>
             <button @click="removeArtist(artist.id)" >
                 <svg   xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>            
             </button>
