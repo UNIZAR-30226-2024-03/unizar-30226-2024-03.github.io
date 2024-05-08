6593b123ce54154839aed3ce8efc49a94b70a021
Miguel Aréjula <arejula10@gmail.com>
2024-05-08 07:12:29 +0000

    try netlify deploy

--- playbeat/astro.config.mjs
+++ playbeat/astro.config.mjs
@@ -12,7 +12,10 @@ export default defineConfig({
   // site: 'https://unizar-30226-2024-03.github.io',
   integrations: [tailwind(), react(), svelte(), vue(), auth()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify({
+    cacheOnDemandPages: true,
+    edgeMiddleware: true
+  }),
 
   prefetch: {
     prefetchAll: false
